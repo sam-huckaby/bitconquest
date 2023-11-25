b127541802a1a697db52229ac86601c3a8441c86
Sam Huckaby <samhuckaby@gmail.com>
2023-11-25 13:19:45 -0500

    Set the metadataBase to the root url of the deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Bit Conquest',
   description: 'Showcase your domain collection!',
+  metadataBase: new URL(process.env.NEXT_PUBLIC_ROOT_URL ?? ''),
 }
 
 export default async function RootLayout({
