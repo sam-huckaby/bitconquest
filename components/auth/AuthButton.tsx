389dbc3ae6a6157a1ecf718ee757a9d8c985ba3b
Sam Huckaby <samhuckaby@gmail.com>
2023-11-25 13:14:29 -0500

    experiment with direct import for button

--- components/auth/AuthButton.tsx
+++ components/auth/AuthButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ProfileButton from './ProfileButton'
-import { Button } from '@mui/material'
+import Button from '@mui/material/Button'
 import { Login as LoginIcon } from '@mui/icons-material'
 import { useAuth } from './AuthContext'
 
