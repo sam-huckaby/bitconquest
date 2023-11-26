b28483011708da2f9b0168de40e7763c3d1f71f4
Sam Huckaby <samhuckaby@gmail.com>
2023-11-25 21:53:27 -0500

    Add the logo to the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,6 +23,7 @@ export default function Login() {
 
   return (
     <main className="absolute z-10 top-0 right-0 bottom-0 left-0 flex min-h-full flex-col items-center justify-center p-24 bottom-background">
+      <img src='/bitconquest-logo.png' alt='Bit Conquest Logo' width={100} height={100} />
       <p className='p-4 font-mono'>Login however you want, as long as it&apos;s with GitHub</p>
       <Button className='bg-gray-900 text-white hover:bg-gray-700 dark:border-white dark:border dark:border-solid' onClick={signInWithGithub}>
         <GitHubIcon className='mr-2' /> Login With GitHub
